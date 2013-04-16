cfec954284b4ae8ce590ee17a4aa920321988ae6
pelayo <mf.pelayo@gmail.com>
2013-04-16 13:23:31 +0200

    commit added retina support addon+example

--- example/src/main.mm
+++ example/src/main.mm
@@ -2,6 +2,11 @@
 #include "testApp.h"
 
 int main(){
-	ofSetupOpenGL(1024,768, OF_FULLSCREEN);			// <-------- setup the GL context
-	ofRunApp(new testApp);
+    
+    ofAppiPhoneWindow * iOSWindow = new ofAppiPhoneWindow();
+    iOSWindow->enableRetinaSupport();
+    ofSetupOpenGL(iOSWindow, 1024, 768, OF_FULLSCREEN);
+    
+    ofRunApp(new testApp);
+
 }
